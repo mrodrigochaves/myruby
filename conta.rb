dd1901d6aec8a5e697da92f298512dc1978549ed
mrodrigochaves <dev.marciorodrigo@gmail.com>
2023-02-13 15:18:02 -0300

    my first commit

---
+++ conta.rb
@@ -0,0 +1,33 @@
+require_relative 'cliente'
+
+class Conta 
+
+    attr_reader :numero, :titular
+    attr_accessor :saldo
+
+    def initialize(numero, titular, saldo)
+    @numero = numero
+    @titular = titular
+    @saldo = saldo
+    end
+
+    def sacar(valor)
+        if saldo >= valor
+        self.saldo -= valor 
+           
+        else
+            puts "Não foi possível executar o saque."
+        end
+    end
+
+    def depositar(valor)
+        self.saldo += valor
+    end
+
+    def transferir(conta_destino, valor)
+        if sacar >= valor
+        conta_destino.depositar(valor)
+        else
+            puts "Não existe saldo suficiente"
+    end
+end
