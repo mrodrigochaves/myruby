515add9f3d3c83c42ad1ea165bad615328792191
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-19 22:36:04 -0300

    update arrays

--- arrays.rb
+++ arrays.rb
@@ -16,3 +16,17 @@ alunos[1] = 'Joaquim' # muda o valor da posição 1 do array de "Pedro" para "Jo
 alunos.first() # retorna o valor da primeira posição do array
 alunos.last() # retornar o valor da última posição do array
 alunos.length() # retorna a quantidade de elementos do array
+
+
+
+alunos = ['André', 'Sophia', 'Laura']
+
+notas = [5, 6, 8]
+
+# André tirou nota 5
+# Sophia tirou nota 6
+# Laura tirou nota 8
+# André tirou nota 5
+# Sophia tirou nota 9
+# Laura tirou nota 8
+# Paulo tirou nota 7.5
