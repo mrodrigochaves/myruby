54d5e706a9d36b19df8ff1955012a8831be706f2
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-23 23:51:30 -0300

    update file

--- aritime.rb
+++ aritime.rb
@@ -6,3 +6,16 @@ puts a * b
 puts a / b
 puts a % b
 puts a ** b
+
+
+puts "Digite um valor"
+a = gets.chomp.to_i
+puts "Digite outro valor"
+b = gets.chomp.to_i
+
+puts a + b
+puts a - b
+puts a * b
+puts a / b
+puts a % b
+puts a ** b
