b030f36bcded5a77180007da797368d5914dd125
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-24 00:04:02 -0300

    update strings

--- estudos/strings.rb
+++ estudos/strings.rb
@@ -28,3 +28,43 @@ nome = gets()
 puts('Qual o seu nome?')
 nome = gets().chomp()
 # => “João
+
+
+puts "Digite a sua idade:"
+idade = gets.chomp.to_i
+
+if idade >= 18
+  puts "Você é maior de idade!"
+end
+
+if condicao
+    # trecho de código a ser executado quando a condição for verdadeira
+  else
+    # trecho de código a ser executado quando a condição for falsa
+  end
+
+
+  puts "Digite uma nota de 0 a 100:"
+nota = gets.chomp.to_i
+
+if nota < 0 or nota > 100
+  puts "Nota inválida! Digite uma nota entre 0 e 100!"
+else
+  if nota <= 20
+    puts "Conceito E"
+  else
+    if nota <= 40
+      puts "Conceito D"
+    else
+      if nota <= 60
+        puts "Conceito C"
+      else
+        if nota <= 80
+          puts "Conceito B"
+        else
+          puts "Conceito A"
+        end
+      end
+    end
+  end
+end
