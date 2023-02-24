e560e400085cbc637768e1dcdf31de4c7e2204b2
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-23 23:56:38 -0300

    update hash

--- estudos/hash.rb
+++ estudos/hash.rb
@@ -6,3 +6,12 @@ meu_hash[:altura]
 # 1.82
 meu_hash[:uma_chave_inexistente]
 # nil
+
+hash2.has_key?(:quatro)
+# true
+hash2.include?(:dez)
+# false
+hash1.key?(:um)
+# true
+hash1.member?(:dois)
+# true
